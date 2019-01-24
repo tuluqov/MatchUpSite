714c939beef66d4d33b52505162b08f2b3df703a
Yusup9505 <vashi.abov@mail.ru>
2019-01-24 12:30:12 +0300

    Minor fix

--- Compatibility in FB API/Controllers/AccountController.cs
+++ Compatibility in FB API/Controllers/AccountController.cs
@@ -76,7 +76,8 @@ namespace MatchUp.Controllers
 
             // Сбои при входе не приводят к блокированию учетной записи
             // Чтобы ошибки при вводе пароля инициировали блокирование учетной записи, замените на shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, true, shouldLockout: false);
+
             switch (result)
             {
                 case SignInStatus.Success:
